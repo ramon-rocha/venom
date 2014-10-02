91aee462caabe422e0e1c58e4f103e17a0118913
Ramon Rocha <ra.rocha.jr@gmail.com>
2014-10-02 14:34:36 -0500

    Create skeletal project structure

---
+++ VenomTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VenomTester
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var parser = new Venom.CobraSyntax.CobraParser();
+			Venom.CobraSyntax.ISyntaxNode root;
+			root = parser.ParseTopLevel("../../../../../cobra/trunk/Samples/Notepad.cobra");
+			System.Console.WriteLine(root.ToPrettyString());
+			//root = parser.ParseTopLevel("../../../../../cobra/trunk/Samples/Shapes.cobra");
+			//System.Console.WriteLine(root.ToPrettyString());
+			//root = parser.ParseTopLevel("../../../../../cobra/trunk/Source/Misc/hello.cobra");
+			//System.Console.WriteLine(root.ToPrettyString());
+		}
+	}
+}
